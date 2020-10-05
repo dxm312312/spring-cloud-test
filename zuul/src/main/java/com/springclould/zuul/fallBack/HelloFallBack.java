166148b1df4a4e3a7988e2ba6788730b77c1d886
dxm112234 <dxm112234@126.com>
2020-10-06 06:21:11 +0800

    springclould  zuul回退

---
+++ zuul/src/main/java/com/springclould/zuul/fallBack/HelloFallBack.java
@@ -0,0 +1,73 @@
+package com.springclould.zuul.fallBack;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+@Component
+public class HelloFallBack implements FallbackProvider {
+
+    /**
+     * 表明为哪个微服务提供回退
+     * 服务Id ，若需要所有服务调用都支持回退，返回null 或者 * 即可
+     */
+    @Override
+    public String getRoute() {
+        return "c1";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        //根据异常自己来判断，这里随便写一个
+        System.out.println(route);
+        System.out.println(cause);
+        return response(HttpStatus.NOT_FOUND);
+    }
+
+    private ClientHttpResponse response(final HttpStatus status) {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                //return status;
+                return HttpStatus.BAD_REQUEST;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                //return status.value();
+                return HttpStatus.BAD_REQUEST.value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                //return status.getReasonPhrase();
+                //return HttpStatus.BAD_REQUEST.name();
+                return HttpStatus.BAD_REQUEST.getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                String msg = "{\"msg\":\"服务故障\"}";
+                return new ByteArrayInputStream(msg.getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
