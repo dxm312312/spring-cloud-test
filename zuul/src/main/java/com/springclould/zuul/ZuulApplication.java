573014b639b12d71bfc463749527113a4dda6371
dxm112234 <dxm112234@126.com>
2020-12-04 19:49:05 +0800

    直接调用zuul上的接口

--- zuul/src/main/java/com/springclould/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/springclould/zuul/ZuulApplication.java
@@ -3,13 +3,21 @@ package com.springclould.zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableZuulProxy
+@RestController
 public class ZuulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+	@RequestMapping(value="/zuulhello")
+	public String zuulHello(){
+		return "zuulHelo";
 	}
 
 }
